3dcb895566eaa742c66e9461ee3a89017cb50617
Loli Delgado Lara <dolores.delgado@wizeline.com>
2022-06-20 09:46:56 -0500

    refactor: :lipstick: Add endpoint method in the route itself
    Handling the method accepted from the delivery layer instead of doing it in the controller

--- controller/github_user.go
+++ controller/github_user.go
@@ -1,3 +1,4 @@
+// Package controller provides the communication between the delivery layer and the usecase layer
 package controller
 
 import (
@@ -11,40 +12,45 @@ import (
 	"github.com/unrolled/render"
 )
 
+// GithubUser struct defines the structure to be used in the controller, so that it can be
+// communicated with the usecase and able to render a response to the router
 type GithubUser struct {
 	render *render.Render
 	g      *usecase.GithubUserUseCase
 }
 
+// NewGithubUser initializes the controller
 func NewGithubUser(r *render.Render, g *usecase.GithubUserUseCase) *GithubUser {
 	return &GithubUser{r, g}
 }
 
+// GetGithubUsers fetches all the available github users from the usecase
 func (c *GithubUser) GetGithubUsers(rw http.ResponseWriter, req *http.Request) {
-	switch req.Method {
-	case http.MethodGet:
-		githubUsers, err := c.g.FetchAll()
-		if err != nil {
-			c.render.Text(rw, http.StatusInternalServerError, fmt.Sprintf("This is not fine🔥\nAnd the reason is: %s", err))
-			return
-		}
-		c.render.JSON(rw, http.StatusOK, githubUsers)
+	githubUsers, err := c.g.FetchAll()
+	if err != nil {
+		c.render.Text(rw, http.StatusInternalServerError, fmt.Sprintf("This is not fine🔥\nAnd the reason is: %s", err))
+		return
 	}
 
+	c.render.JSON(rw, http.StatusOK, githubUsers)
 }
 
+// GetGithubUserById fetches the matching github user filtered by the requested id
 func (c *GithubUser) GetGithubUserById(rw http.ResponseWriter, req *http.Request) {
 	vars := mux.Vars(req)
 	id := vars["id"]
+
 	numericId, err := strconv.Atoi(id)
 	if err != nil {
 		c.render.Text(rw, http.StatusBadRequest, "invalid id")
 		return
 	}
+
 	githubUser, err := c.g.GetById(numericId)
 	if err != nil {
 		c.render.Text(rw, http.StatusInternalServerError, fmt.Sprintf("This is not fine🔥\nAnd the reason is: %s", err))
 		return
 	}
+
 	c.render.JSON(rw, http.StatusOK, githubUser)
 }
